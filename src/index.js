aabc69ce520e369d2ef9d50272adc3565336a6f0
Agnieszka Draganczyk <Agnieszka.127@interia.pl>
2023-09-09 15:54:44 +0200

    style swiper

--- src/index.js
+++ src/index.js
@@ -1,30 +1,22 @@
 
 import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.mjs'
-        let labels = ['&#9733', '&#9733', '&#9733'];
+        let labels = ['&#xB7;', '&#xB7;', '&#xB7;'];
+        
         let swiper = new Swiper(".mySwiper", {
-          centeredSlides: true,
+        centeredSlides: true,
           centerInsufficientSlides: true,
-        //   effect: 'creative',
-        //   creativeEffect: {
-        //     prev: {
-        //      translate: ['-100%', 0, 0],
-        //     },
-        //     next: {
-        //         translate: ['100%', 0, 0],
-        //     },
-        //   },
-
-
+          effect: 'creative',
+          creativeEffect: {
+            prev: {
+             translate: ['-150%', 0, 0],
+            },
+            next: {
+                translate: ['150%', 0, 0],
+            },
+          },
             grabCursor: true,
             centeredSlides: true,
             slidesPerView: "auto",
-            // coverflowEffect: {
-            //     rotate: 50,
-            //     stretch: 0,
-            //     depth: 100,
-            //     modifier: 1,
-            //     slideShadows: true,
-            // },
             pagination: {
                 el: ".swiper-pagination",
                 clickable: true,
@@ -43,4 +35,6 @@ import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.mjs
                 bulletClass: 'my-custom-pagination-item',
                 bulletActiveClass: 'my-custom-pagination-active'
             },
-        });
+        });  
+      
+        
