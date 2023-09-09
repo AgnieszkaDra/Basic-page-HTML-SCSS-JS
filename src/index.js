b9ac45639aa5e70045bf1b690b9cbe542d6b3611
Agnieszka Draganczyk <Agnieszka.127@interia.pl>
2023-09-09 13:49:49 +0200

    try to style swiper

--- src/index.js
+++ src/index.js
@@ -1,41 +1,46 @@
-console.log('swiper')
 
-import Swiper from 'swiper';
-// import Swiper styles
-import 'swiper/css';
+import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.mjs'
+        let labels = ['&#9733', '&#9733', '&#9733'];
+        let swiper = new Swiper(".mySwiper", {
+          centeredSlides: true,
+          centerInsufficientSlides: true,
+        //   effect: 'creative',
+        //   creativeEffect: {
+        //     prev: {
+        //      translate: ['-100%', 0, 0],
+        //     },
+        //     next: {
+        //         translate: ['100%', 0, 0],
+        //     },
+        //   },
 
-import { Navigation, Pagination } from 'swiper/modules';
-// const swiper = new Swiper(...);
-// let labels = ['&#9733', '&#9733', '&#9733'];
-const swiper = new Swiper(".mySwiper", {
 
-    effect: "coverflow",
-    grabCursor: true,
-    centeredSlides: true,
-    slidesPerView: "auto",
-    coverflowEffect: {
-        rotate: 50,
-        stretch: 0,
-        depth: 100,
-        modifier: 1,
-        slideShadows: true,
-    },
-    Pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-        renderBullet: function (index, className) {
+            grabCursor: true,
+            centeredSlides: true,
+            slidesPerView: "auto",
+            // coverflowEffect: {
+            //     rotate: 50,
+            //     stretch: 0,
+            //     depth: 100,
+            //     modifier: 1,
+            //     slideShadows: true,
+            // },
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+                renderBullet: function (index, className) {
 
-            if (labels[index] == null) {
-                className = 'null'
-                return '<div class="' + className + '">' + '' +
-                    '</div>';
-            } else {
-                return '<div class="' + className + '">' + (labels[index]) +
-                    '</div>';
-            }
+                    if (labels[index] == null) {
+                        className = 'null'
+                        return '<div class="' + className + '">' + '' +
+                            '</div>';
+                    } else {
+                        return '<div class="' + className + '">' + (labels[index]) +
+                            '</div>';
+                    }
 
-        },
-        bulletClass: 'my-custom-pagination-item',
-        bulletActiveClass: 'my-custom-pagination-active'
-    },
-});
+                },
+                bulletClass: 'my-custom-pagination-item',
+                bulletActiveClass: 'my-custom-pagination-active'
+            },
+        });
